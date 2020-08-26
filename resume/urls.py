7efe6992196f14e94a4617972410cdd07e058ed3
Vadim <callofdutz@mail.ru>
2020-08-26 18:34:39 +0300

    fixed ckeditor upload images

--- resume/urls.py
+++ resume/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.views.generic import RedirectView
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf.urls import handler404
 from mainapp import views as main_views
+from django.views.static import serve
 from django.views.generic.base import TemplateView
 from .sitemap import ProjectsSitemap, StaticSitemap
 from django.contrib.sitemaps import views, GenericSitemap
@@ -39,10 +40,9 @@ urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/img/icon.ico', permanent=True)),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('sitemap.xml', views.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 
 if settings.ADMIN_ENABLED:
-    urlpatterns += [path(os.environ.get("ADMIN_PATH"), admin.site.urls)]
-
-
+    urlpatterns += [path(os.environ.get("ADMIN_PATH"), admin.site.urls),]
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),]
